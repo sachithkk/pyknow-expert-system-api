52ea2e5f78c1015c4d0c4d3fbae41f47b274cc15
sajith priyankara <sajith1995s@gmail.com>
2019-10-30 12:35:20 +0530

    add cpu controller

--- api/__init__.py
+++ api/__init__.py
@@ -5,9 +5,12 @@ from app import flaskAppInstance
 from .LaptopController import LaptopController
 from .CommentsController  import CommentsController
 from .GameController import GameController
+from .CPUController import CPUController
 
 restServer = Api(flaskAppInstance)
 
 restServer.add_resource(LaptopController, "/api-py-techRing/Laptop")
 restServer.add_resource(CommentsController, "/api-py-techRing/analyze_comments")
 restServer.add_resource(GameController, "/api-py-techRing/scrape_game")
+restServer.add_resource(CPUController, "/api-py-techRing/cpu_points")
+
