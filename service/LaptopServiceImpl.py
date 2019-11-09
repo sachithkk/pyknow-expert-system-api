f360a5f7ae3fa542112460a4754defaac0d68598
sachithkk <tharakakk@gmail.com>
2019-11-09 14:44:02 +0530

    fixed error

--- service/LaptopServiceImpl.py
+++ service/LaptopServiceImpl.py
@@ -20,11 +20,8 @@ class LaptopService(KnowledgeEngine):
     flaskAppInstance.config['MONGO_URI'] = 'mongodb://localhost:27017/techRingdb'
     mongo = PyMongo(flaskAppInstance);
 
-<<<<<<< HEAD
     #result = mongo.db.cpu_mode_point.find();
-=======
-    result = mongo.db.x.find();
->>>>>>> 37078f29e8123cc490c49ae3988fd29f8787d756
+    # result = mongo.db.x.find();
 
     compational_power_point = 0
 
